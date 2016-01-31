57ff01eef01ba670e5a73956b488d08661ceb0eb
Gustavo Falco <comfortablynumb84@gmail.com>
2016-01-31 10:11:28 -0300

    Added base files.

---
+++ src/Service.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the activemq package.
+ *
+ * (c) Gustavo Falco <comfortablynumb84@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IronEdge\Component\ActiveMq;
+
+
+/*
+ * @author Gustavo Falco <comfortablynumb84@gmail.com>
+ */
+class Service
+{
+
+}
